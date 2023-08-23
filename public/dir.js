f5e54da6d356c69c4f11bf9d38a5a1b2efb4dd10
Al Muhit Muhtadi <113031079+Muhtadi13@users.noreply.github.com>
2023-08-23 23:22:04 +0600

    map backend connection by amimulamim

--- public/dir.js
+++ public/dir.js
@@ -43,11 +43,26 @@ function failure(){
 
 }
 
+// function initMap(){
+//   const until=setInterval(() => {
+//     if(coords===undefined){
+//       return;
+//     }
+//     else{
+//       initializeMap();
+//       clearInterval(until);
+//     }
+    
+//   }, 1000);
+
+  
+// }
 
 function initMap() {
+    //alert(1);
     const map = new google.maps.Map(document.getElementById("map"), {
       mapTypeControl: false,
-      
+      // center:coords
       center: { lat: 23.7266616, lng: 90.381105 } ,
       zoom: 13,
     });
@@ -223,19 +238,19 @@ function initMap() {
               // Assuming you have a form and a button with the id "submit-button"
 
   const req_data = {
-    origin: 'api req='+'https://maps.googleapis.com/maps/api/geocode/json?place_id='+this.originPlaceId+'&key=AIzaSyBPcblOBNzRF6zi8xrbXLdrTWTPn7_P2JA',
-    destination: 'api req='+'https://maps.googleapis.com/maps/api/geocode/json?place_id='+this.destinationPlaceId+'&key=AIzaSyBPcblOBNzRF6zi8xrbXLdrTWTPn7_P2JA',
+    origin: 'https://maps.googleapis.com/maps/api/geocode/json?place_id='+this.originPlaceId+'&key=AIzaSyBPcblOBNzRF6zi8xrbXLdrTWTPn7_P2JA',
+    destination: 'https://maps.googleapis.com/maps/api/geocode/json?place_id='+this.destinationPlaceId+'&key=AIzaSyBPcblOBNzRF6zi8xrbXLdrTWTPn7_P2JA',
     travelMode: this.travelMode,
     distance: response.routes[0].legs[0].distance.text,
     duration: response.routes[0].legs[0].duration.text
   
   };
-  const sendresponse = await fetch("/user/riderequest/currentreq", {
+  const sendresponse = await fetch("/user/requests", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: req_data,
+    body: JSON.stringify(req_data),
   });
 
   if (sendresponse.ok) {
--- router/User/TripRequests/requests.js
+++ router/User/TripRequests/requests.js
@@ -2,19 +2,87 @@
 const express=require('express');
 const DB_trips=require('../../../Database/DB-user-trips');
 const { json } = require('body-parser');
+//const dir=require('../../../public/dir')
 
 //creating routers
 const router=express.Router({mergeParams:true});
 
 router.get('/',async(req,res) =>{
-    if(req.users==null){
+    if(req.user==null){
+        console.log('get e user nai');
         return res.redirect('/user/login');
     }
+    console.log('get e user ase');
 
-    const requestsNearby=await DB_trips.makeTripRequests();
-    //will do front end here . for now in json format
-    console.log(json(requestsNearby.data));
-    res.status(200).json(requestsNearby.data);
+    // const requestsNearby=await DB_trips.makeTripRequests();
+    // //will do front end here . for now in json format
+    // console.log(json(requestsNearby.data));
+    // res.status(200).json(requestsNearby.data);
+    console.log(req.body.data);
+    // res.render('layout.ejs', {
+    //     user:req.user,
+    //     body:['bookPage'],
+    //     title:'Books',
+    //     navbar:1,
+    //     book:booksResult[0],
+    //     reviews:reviews,
+    //     canReview:canReview,
+    //     hasReviewd:hasReviewd,
+    //     hasAddedWish:addedToWishList
+    // });
+});
+router.get('/current',async(req,res) =>{
+    if(req.user==null){
+        console.log('get e user nai');
+        return res.redirect('/user/login');
+    }
+    console.log('get e user ase');
+
+    // const requestsNearby=await DB_trips.makeTripRequests();
+    // //will do front end here . for now in json format
+    // console.log(json(requestsNearby.data));
+    // res.status(200).json(requestsNearby.data);
+    console.log(req.body.data);
+    // res.render('layout.ejs', {
+    //     user:req.user,
+    //     body:['bookPage'],
+    //     title:'Books',
+    //     navbar:1,
+    //     book:booksResult[0],
+    //     reviews:reviews,
+    //     canReview:canReview,
+    //     hasReviewd:hasReviewd,
+    //     hasAddedWish:addedToWishList
+    // });
+});
+
+router.post('/',async(req,res) =>{
+    if(req.user==null){
+        console.log('post e user nai');
+        return res.redirect('/user/login');
+    }
+    console.log('post e user ase');
+    
+    console.log('dir theke aslam',req.body);
+
+
+    const origin=await fetch(req.body.origin);
+    const json=await origin.json();
+    console.log(json.results[0].geometry);
+
+    // const sendresponse = await fetch("/user/requests", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: req_data,
+    //   });
+
+
+    // const requestsNearby=await DB_trips.makeTripRequests();
+    // //will do front end here . for now in json format
+    // console.log(json(requestsNearby.data));
+    // res.status(200).json(requestsNearby.data);
     // res.render('layout.ejs', {
     //     user:req.user,
     //     body:['bookPage'],
