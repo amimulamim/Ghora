b005a90bca9fa5dd5529b29cad198820102e8222
amimulamim <142148987+amimulamim@users.noreply.github.com>
2023-09-05 23:21:13 +0600

    much updates

--- router/Driver/completed.js
+++ router/Driver/completed.js
@@ -3,37 +3,20 @@ const express = require('express');
 //const DB_trips = require('../../../Database/DB-driver-trips');
 const { json } = require('body-parser');
 //const DB_driver = require('../../../Database/DB-driver-api');
-<<<<<<< Updated upstream
 const DB_trips = require('../../Database/DB-trips-api');
 const payment = require('../../Database/DB-payment-api');
-=======
-const DB_trips=require('../../Database/DB-trips-api');
-const payment=require('../../Database/DB-payment-api');
->>>>>>> Stashed changes
+
 
 
 //const processrequest = require('../../Map/processRequest');
 //creating routers
 const router = express.Router({ mergeParams: true });
 
-<<<<<<< Updated upstream
 router.post('/', async (req, res) => {
-=======
-router.post('/',async(req,res)=>{
->>>>>>> Stashed changes
     if (req.driver == null) {
         return res.redirect('/driver/login');
     }
     const running = await DB_trips.runningOfDriver(req.driver.ID);
-<<<<<<< Updated upstream
-    if (running.length === 0) {
-        return res.redirect('/driver');
-    }
-    else {
-        await DB_trips.createTripHistory(running[0]);
-        console.log('redirecting to complete');
-        return res.redirect('/driver/completed');
-=======
     if(running.length===0){
         res.redirect('/driver');
     }
@@ -50,14 +33,11 @@ router.post('/',async(req,res)=>{
    
 
     // });
->>>>>>> Stashed changes
     }
 
 
 });
 
-
-<<<<<<< Updated upstream
 router.get('/', async (req, res) => {
     if (req.driver == null) {
         return res.redirect('/driver/login');
@@ -79,30 +59,6 @@ router.get('/', async (req, res) => {
             trip_info: completed[0],
             driver: req.driver,
             payment: payment_details
-=======
-router.get('/',async(req,res)=>
-{
-    if (req.driver == null) {
-        return res.redirect('/driver/login');
-    }
-    const completed=await DB_trips.completedTripDetailsofDriver(req.driver.ID);
-
-    if(completed.length == 0) {
-        console.log('nothing recently completed');
-        res.redirect('/driver');
-    }
-    else{
-        console.log('ready to render completed : ',completed[0]);
-        console.log('trip history: ' , completed[0]);
-        const payment_details=await payment.getPaymentDetails(completed[0].TR_ID);
-        console.log('payment details in driver completed: ' , payment_details[0]);
-        res.render('driverLayout.ejs',{
-            title:'completed',
-            page:['driverCompleted'],
-            trip_info:completed[0],
-            driver:req.driver,
-            payment:payment_details
->>>>>>> Stashed changes
 
 
 
